36ef334a039fe638a27dd631619af5c1a9ec91d3
hflicka <hflicka@gmail.com>
2011-09-23 12:24:27 +0200

    added some more literature

--- htdocs/links.php
+++ htdocs/links.php
@@ -1,4 +1,4 @@
-<?php include "includes/head.php"; ?>
+﻿<?php include "includes/head.php"; ?>
 
 <h2>Links and References</h2>
 
@@ -12,11 +12,34 @@
 <h3>Referenced Work</h3>
 
 <p><ul>
-<li>Hripcsak, George<br />
-  Writing Arden Syntax Medical Logic Modules<br />
+<li>Hripcsak, George<br>
+  <em>Writing Arden Syntax Medical Logic Modules</em><br>
   Comput Biol Med, Center for Medical Informatics, 
   Columbia-Presbyterian Medical Center, 
-  New York, NY 10032, USA., 1994, Vol. 24(5), pp. 331-363</li>
+  New York, NY 10032, USA., 1994, Vol. 24(5), pp. 331-363
+</li>
+<li>
+  <a href="http://sablecc.org/">SableCC Parser Generator</a>
+</li>
+<li>
+  Song, B.; Wolf, K.-H.; Gietzelt, M.; Scharaa, O. A.; Tegtbur, U.; Haux, R. &amp; Marschollek, M.<br>
+  <em>Decision support for teletraining of COPD patients.</em><br>
+  Methods Inf Med, Bianying Song, Peter L. Reichertz Institute for Medical Informatics, University of Braunschweig - Institute of Technology and Hannover Medical School, M&uuml;hlenpfordtstrasse 23, 38106 Braunschweig, Germany, 2010, 49, 96-102
+</li>
+<li>
+  Spates, R. P. &amp; Aller, K. C.<br>
+  <em>One vendor's experience: preliminary development 
+  of a reminder system based on the Arden Syntax</em><br> 
+  Comput Biol Med, Company, Amherst, MA 01002, USA., 1994, 24, 371-375
+</li>
+<li>
+  Starren, J. B.; Hripcsak, G.; Jordan, D.; Allen, B.; Weissman, C. &amp; Clayton, P. D.<br>
+  <em>Encoding a post-operative coronary artery bypass 
+  surgery care plan in the Arden Syntax</em><br>
+  Comput Biol Med, Center for Medical Informatics, 
+  Columbia-Presbyterian Medical Center, 
+  New York, NY 10032, USA., 1994, 24, 411-417
+</li>
 </ul></p>
 
 
