b56278973cd4ffd4cb091a256d250fb7f6eef51b
hflicka <hflicka@gmail.com>
2011-09-23 14:32:39 +0200

    added more referenced work

--- htdocs/links.php
+++ htdocs/links.php
@@ -12,18 +12,66 @@
 <h3>Referenced Work</h3>
 
 <p><ul>
-<li>Hripcsak, George<br>
+<!--
+<li>
+  <br>
+  <em></em><br>
+  
+</li> 
+-->
+<li>
+  Gao, X.; Johansson, B.; Shahsavar, N.; Arkad, K.; Ahlfeldt, H. &amp; Wigertz, O.<br>
+  <em>Pre-compiling medical logic modules into C++ in building medical decision support systems</em><br>
+  Comput Methods Programs Biomed, Department of Medical Informatics, Link&ouml;ping University, Sweden., 1993, 41, 107-119
+</li>
+<li>
+  Grunwald, D.<br>
+  <em>Arden2ByteCode - Open source Arden Syntax compiler implementation</em><br>
+  <a href="https://github.com/dgrunwald/arden2bytecode">https://github.com/dgrunwald/arden2bytecode</a>
+</li>
+<li>
+  Hripcsak, George<br>
   <em>Writing Arden Syntax Medical Logic Modules</em><br>
   Comput Biol Med, Center for Medical Informatics, 
   Columbia-Presbyterian Medical Center, 
   New York, NY 10032, USA., 1994, Vol. 24(5), pp. 331-363
 </li>
 <li>
+  Karadimas, H. C.; Chailloleau, C.; Hemery, F.; Simonnet, J. &amp; Lepage, E.<br>
+  <em>Arden/J: an architecture for MLM execution on the Java platform</em><br>
+  J Am Med Inform Assoc, Biostatistics and Medical Informatics Department, 
+  Henri Mondor Hospital, Creteil, France, 2002, 9, 359-368
+</li>
+<li>
+  Kuhn, R. A. &amp; Reider, R. S.<br>
+  <em>A C++ framework for developing Medical Logic Modules and an Arden Syntax compiler</em><br>
+  Comput Biol Med, IBM Corporation, Bethesda, MD 20817, USA., 1994, 24, 365-370
+</li>
+<li>
+  Ludemann, P.<br>
+  <em>Mid-term report on the Arden Syntax in a clinical event monitor</em><br>
+  Comput Biol Med, Institute for Medical Informatics, 
+  Ameritech Knowledge Data, Palo Alto, CA 94301, USA., 1994, 24, 377-383
+</li>
+<li>
+  McCallum, G.<br>
+  <em>EGADSS: A Clinical Decision Support System for use 
+  in a Service- oriented Architecture</em><br>
+  Computer Science, University of Victoria, 2006
+</li>
+<li>
+  McCauley, B.; Young, I.; Clark, I. &amp; Peters, M.<br>
+  <em>Incorporation of the Arden Syntax 
+  within the reimplementation of a closed-loop decision support system</em><br>
+  Comput Biomed Res, Wolfson Computer Laboratory, 
+  University of Birmingham, United Kingdom., 1996, 29, 507-518
+</li>
+<li>
   <a href="http://sablecc.org/">SableCC Parser Generator</a>
 </li>
 <li>
   Song, B.; Wolf, K.-H.; Gietzelt, M.; Scharaa, O. A.; Tegtbur, U.; Haux, R. &amp; Marschollek, M.<br>
-  <em>Decision support for teletraining of COPD patients.</em><br>
+  <em>Decision support for teletraining of COPD patients</em><br>
   Methods Inf Med, Bianying Song, Peter L. Reichertz Institute for Medical Informatics, University of Braunschweig - Institute of Technology and Hannover Medical School, M&uuml;hlenpfordtstrasse 23, 38106 Braunschweig, Germany, 2010, 49, 96-102
 </li>
 <li>
