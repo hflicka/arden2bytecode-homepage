cb6f418f8d347a144dcf2b3d9682c336cceb6dcb
Hannes Flicka <hflicka@gmail.com>
2011-09-30 15:43:32 +0200

    included sub-menu of documents

--- htdocs/includes/menu.php
+++ htdocs/includes/menu.php
@@ -1,7 +1,14 @@
+<?php
+	include_once "docs.php";
+?>
 <ul>
 <li><a href="/">About</a></li>
 <li><a href="/download">Downloads</a></li>
-<li><a href="/docs/">Documentation</a></li>
+<li><a href="/docs/">Documentation</a>
+<?php 
+	render_file_list(); 
+?>
+</li>
 <li><a href="/links">Links</a></li>
 <li><a href="/source">Source Code</a></li>
 <li><a href="/issues">Bug reports</a></li>
