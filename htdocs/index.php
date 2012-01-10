5c0a1951a48d4676f9b2a00abde5da07c747b641
Hannes Flicka <hflicka@gmail.com>
2012-01-10 12:46:04 +0100

    fixed typo

--- htdocs/index.php
+++ htdocs/index.php
@@ -55,7 +55,7 @@ a binary package for your platform and read the wiki page
 >Getting started with arden2bytecode</a></p>
 
 <p>Do you have problems to get started or ideas how to improve Arden2ByteCode?
-Then, <a href=contact">please contact us!</a><br>
+Then, <a href="/contact">please contact us!</a><br>
 You might wish to submit your issues, ideas or proposals via the
 <a href="https://github.com/hflicka/arden2bytecode/issues/new">Arden2ByteCode issue tracker</a>.</p>
 
