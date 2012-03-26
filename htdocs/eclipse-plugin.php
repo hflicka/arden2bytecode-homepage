09f4f1caf1ea28564c99dd782089767f3369e643
hflicka <hflicka@gmail.com>
2012-03-26 15:12:23 +0200

    updated installation instructions with new software site installation path

--- htdocs/eclipse-plugin.php
+++ htdocs/eclipse-plugin.php
@@ -6,15 +6,29 @@
 
 <h3>Installation Instructions</h3>
 
+<p>You can choose between two ways of installing:</p>
+
+<ul>
+<li>via Eclipse Marketplace (recommended, but slow)</li>
+<li>via Eclipse Update Site (more complicated, but faster)</li>
+</ul>
+
 <h4>via Eclipse Marketplace</h4>
 
-<p>This way of installing will be available once the Marketplace content is confirmed as valid.</p>
+<p>To install via Eclipse Marketplace, follow these steps:</p>
+<ol>
+<li>Drag and drop the following Button onto a running Eclipse window to install the Arden Syntax Tools:
+<a href="http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=209263" 
+   title="Drag and drop into a running Eclipse Indigo workspace to install Arden Syntax Tools"> 
+	<img src="http://marketplace.eclipse.org/misc/installbutton.png" />
+</a>
+</li>
+<li>Follow the installation procedure. When asked if you want to install unsigned software, click OK.</li>
+</ol>
 
 <h4>via Eclipse Update Site</h4>
 
-<p>As of February 2012 this is the only way of installing the plugin.</p>
-
-<p>To install, follow these steps:</p>
+<p>To install via the <a href="http://arden2bytecode.sf.net/eclipse/update/">Arden2ByteCode Update Site</a>, follow these steps:</p>
 <ol>
 <li>Start a recent version of the Eclipse IDE.<br>
 If you don't have Eclipse yet, you may download the Eclipse IDE 
@@ -29,7 +43,7 @@ The name you enter for that site does not matter.<br>
 Note that the loading of the Software Site content may take a 
 while as the Eclipse servers are slow. During this time, you will see a "Pending..." notice.</li>
 <li>Mark the "Arden Syntax Tools" to be installed. Click next.</li>
-<li>Follow the installation procedure.</li>
+<li>Follow the installation procedure. When asked if you want to install unsigned software, click OK.</li>
 <li>Once the Arden Syntax Editor is installed, Eclipse will prompt you if you 
 want to add the xText Nature to your project when opening or creating an MLM (*.mlm) file. <br>
 Choose "yes" to activate the Arden Syntax Editor.</li>
