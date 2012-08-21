43bf867cbbb3a70fd3fd7bc62023845f1122a335
Hannes Flicka <hflicka@gmail.com>
2012-08-21 17:57:21 +0200

    tweaked developerdocs page

--- htdocs/developerdocs.php
+++ htdocs/developerdocs.php
@@ -1,6 +1,7 @@
 <?php include "includes/head.php"; ?>
 
 <div id="devdocs">
+
 <h1>Arden2ByteCode Developer Documentation</h1>
 
 <p>This file is a collection of the various 
@@ -8,6 +9,7 @@ README.md <a href="http://en.wikipedia.org/wiki/Markdown">Markdown</a>
 files contained in the
 Arden2ByteCode GitHub repositories</p>
 
+<div id="includes">
 <?php
     $devdocs = "includes/developerdocs.html";
     if (is_file($devdocs)) {
@@ -16,6 +18,7 @@ Arden2ByteCode GitHub repositories</p>
 	    echo "<p>No developer documentation found. Generate with <code>makedocs</code> script.</p>";
 	}
 ?>
+</div>
 </div>
 
 <?php include "includes/foot.php"; ?>
