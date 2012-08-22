da66efdb3415dbe81dc700497e0302751870a74f
Hannes Flicka <hflicka@gmail.com>
2012-08-22 16:15:21 +0200

    added issues of ardensyntax-eclipse-plugin as well

--- htdocs/issues.php
+++ htdocs/issues.php
@@ -6,9 +6,13 @@
 <a href="https://github.com/hflicka/arden2bytecode/issues/new"
 >https://github.com/hflicka/arden2bytecode/issues/new</a>.</p>
 
-<p>Issues of Arden2ByteCode at GitHub are:</p>
+<h3>Issues of <a href="https://github.com/hflicka/arden2bytecode">Arden2ByteCode</a> at GitHub</h3>
 
-<ul id="issues"><li style="list-style-type: none;">Loading (requires JavaScript)...</li></ul>
+<ul id="arden2bytecode" class="issues"><li style="list-style-type: none;">Loading (requires JavaScript)...</li></ul>
+
+<h3>Issues of the <a href="https://github.com/hflicka/ardensyntax-eclipse-plugin">Arden Syntx Eclipse plugin</a> at GitHub</h3>
+
+<ul id="arden2bytecode" class="issues"><li style="list-style-type: none;">Loading (requires JavaScript)...</li></ul>
 
 <script type="text/javascript">//<![CDATA[
 $('head').append($('<script />', {
@@ -18,21 +22,35 @@ $('head').append($('<script />', {
 //]]>
 </script>
 <script type="text/javascript">//<![CDATA[
-$(function() {
-    var converter = new Showdown.converter();
-    $.ajax({
-		url: 'https://api.github.com/repos/hflicka/arden2bytecode/issues',
-		dataType: 'jsonp'
-	}).done(function(result) {
-		$('#issues').empty();
-		$.each(result.data, function(index, issue) {
-			$('#issues').append(
-				'<li><span class="issuetitle"><a href="' + issue.html_url
+
+var converter = new Showdown.converter();
+	
+function formatIssue(issue) {
+	return '<li class="issue"><span class="issuetitle"><a href="' + issue.html_url
 				+ '">' + issue.title 
 				+ '</a> (' + issue.state + ') '
 				+ '<span class="issueowner"> - posted by: <a href="' + issue.user.url + '">' 
 				+ issue.user.login + '</a></span></span>' 
-				+ '<div class="issuebody">' + converter.makeHtml(issue.body) + '</div></li>');
+				+ '<div class="issuebody">' + converter.makeHtml(issue.body) + '</div></li>';
+}
+
+$(function() {
+    $.ajax({
+		url: 'https://api.github.com/repos/hflicka/arden2bytecode/issues',
+		dataType: 'jsonp'
+	}).done(function(result) {
+		$('#arden2bytecode').empty();
+		$.each(result.data, function(index, issue) {
+			$('#arden2bytecode').append(formatIssue(issue));
+		});
+	});
+	$.ajax({
+		url: 'https://api.github.com/repos/hflicka/ardensyntax-eclipse-plugin/issues',
+		dataType: 'jsonp'
+	}).done(function(result) {
+		$('#eclipseplugin').empty();
+		$.each(result.data, function(index, issue) {
+			$('#eclipseplugin').append(formatIssue(issue));
 		});
 	});
 });
