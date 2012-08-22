a0777f09d4f6f0a6a8761f376a1d9b53ae4d96e5
Hannes Flicka <hflicka@gmail.com>
2012-08-22 15:17:29 +0200

    added issue loader to web site

--- htdocs/issues.php
+++ htdocs/issues.php
@@ -6,4 +6,33 @@
 <a href="https://github.com/hflicka/arden2bytecode/issues/new"
 >https://github.com/hflicka/arden2bytecode/issues/new</a>.</p>
 
+<p>Currently active issues are:</p>
+
+<ul id="issues"><li style="list-style-type: none;">Loading (requires JavaScript)...</li></ul>
+
+<script type="text/javascript">
+$('head').append($('<script />', {
+  src: 'github-flavored-markdown.js',
+  type: 'text/javascript'
+}));
+</script>
+<script type="text/javascript">
+$(function() {
+    var converter = new Showdown.converter();
+    $.ajax({
+		url: 'https://api.github.com/repos/hflicka/arden2bytecode/issues',
+		dataType: 'jsonp'
+	}).done(function(result) {
+		$('#issues').empty();
+		$.each(result.data, function(index, issue) {
+			$('#downloads').append(
+				'<li><span class="issuetitle"><a>href="' + issue.html_url
+				+ '">' + issue.title 
+				+ '</a> (' + issue.state + ')</span>' 
+				+ '<div class="issuebody">' + converter.makeHtml(issue.body) + '</div></li>');
+		});
+	});
+});
+</script>
+
 <?php include "includes/foot.php"; ?>
